bd7a5902d290929dc9db4609fbc86e49b402967a
Batschko <45481788+Batschko@users.noreply.github.com>
2024-06-20 23:30:37 +0200

    use new price methods

--- src/main/java/de/batschko/tradeupproject/TradeUpProjectApplicationCMD.java
+++ src/main/java/de/batschko/tradeupproject/TradeUpProjectApplicationCMD.java
@@ -4,7 +4,7 @@ import de.batschko.tradeupproject.db.query.QRInitQueries;
 import de.batschko.tradeupproject.db.query.QRTradeUp;
 import de.batschko.tradeupproject.tradeup.Generator;
 import de.batschko.tradeupproject.utils.CSMoneyUtils;
-import de.batschko.tradeupproject.webfetchers.CSGOBackpackApi;
+import de.batschko.tradeupproject.utils.TradeUpUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -85,8 +85,8 @@ public class TradeUpProjectApplicationCMD {
 		cmdListNames.put("1", "loadInitialCaseCollection");
 		cmdListNames.put("2", "saveStashHolderToDatabase");
 		cmdListNames.put("3", "saveCS2SkinToDatabase");
-		cmdListNames.put("4", "setSkinPricesSpecialChars");
-		cmdListNames.put("5", "setSkinPrices");
+		cmdListNames.put("4", "priceUpdateByDate");
+		cmdListNames.put("5", "priceUpdateMissing");
 		cmdListNames.put("6", "floatId 2 generateSingleCollTradeups");
 		cmdListNames.put("7", "floatId 4 generateSingleCollTradeups");
 		cmdListNames.put("8", "false createTradeUpSkins");
@@ -98,14 +98,18 @@ public class TradeUpProjectApplicationCMD {
 		cmdList.put("1", QRInitQueries::loadInitialCaseCollection);
 		cmdList.put("2", QRInitQueries::generateAndSaveStashHolderToDatabase);
 		cmdList.put("3", QRInitQueries::generateAndSaveCS2SkinToDatabase);
-		cmdList.put("4", () -> CSGOBackpackApi.setSkinPricesSpecialChars(21, false));
-		cmdList.put("5", () -> CSGOBackpackApi.setSkinPrices(Integer.MAX_VALUE,21, false));
+		cmdList.put("4", TradeUpUtils::priceUpdateByDate);
+		cmdList.put("5", TradeUpUtils::priceUpdateMissing);
 		cmdList.put("6", () -> Generator.generateSingleCollTradeUps(2));
 		cmdList.put("7", () -> Generator.generateSingleCollTradeUps(4));
 		cmdList.put("8", () -> QRTradeUp.createTradeUpSkins(false));
 		cmdList.put("9", () -> QRTradeUp.createTradeUpSkins(true));
-		cmdList.put("0", CSGOBackpackApi::calculateAllTradeUpsBackpackPrice);
+		cmdList.put("0", TradeUpUtils::calculateAllTradeUps);
 
+
+		// cmdList.put("0", CSGOBackpackApi::calculateAllTradeUpsBackpackPrice);
+		// cmdList.put("4", () -> CSGOBackpackApi.setSkinPricesSpecialChars(21, false));
+		// cmdList.put("5", () -> CSGOBackpackApi.setSkinPrices(Integer.MAX_VALUE,21, false));
 
 		Scanner in = new Scanner(System.in);
 		String input = "";
@@ -132,13 +136,11 @@ public class TradeUpProjectApplicationCMD {
 		cmdListNames.put("h", "Help");
 		cmdListNames.put("q", "go back");
 		cmdListNames.put("1", "runCSMoneyFilteredUrlMenu");
-	//	cmdListNames.put("2", "runCSMoneyPriceCheckMenu");
 
 		Map<String, Runnable> cmdList = new HashMap<>();
 		cmdList.put("h", () -> printCmdNames(cmdListNames));
-		cmdList.put("q", () -> System.out.println());
+		cmdList.put("q", System.out::println);
 		cmdList.put("1", TradeUpProjectApplicationCMD::runCSMoneyFilteredUrlMenu);
-	//	cmdList.put("2", TradeUpProjectApplicationCMD::runCSMoneyPriceCheckMenu);
 		Scanner in = new Scanner(System.in);
 		String input = "";
 		while (!input.equals("q")) {
@@ -164,11 +166,4 @@ public class TradeUpProjectApplicationCMD {
 		CSMoneyUtils.getCSMoneyUrlFiltered(tupId);
 	}
 
-/*	static void runCSMoneyPriceCheckMenu(){
-		int tupId = -1;
-		Scanner in = new Scanner(System.in);
-		System.out.print("TradeUp id: ");
-		tupId = in.nextInt();
-		CSMoneyUtils.csMoneyPriceCheck(tupId);
-	}*/
 }
