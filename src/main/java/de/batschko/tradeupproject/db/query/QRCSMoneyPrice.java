bf5132a8dc5d84839b466237efe29c44bd0098fb
Batschko <45481788+Batschko@users.noreply.github.com>
2024-06-21 21:38:42 +0200

    remove duplicate qr tup/csm methods
    remove duplicate qr tup/csm methods
    
    remove duplicate qr tup/csm methods

--- src/main/java/de/batschko/tradeupproject/db/query/QRCSMoneyPrice.java
+++ src/main/java/de/batschko/tradeupproject/db/query/QRCSMoneyPrice.java
@@ -42,8 +42,7 @@ public class QRCSMoneyPrice extends QueryRepository{
         super(dslContext);
     }
 
-
-
+    
 
 
     public static Result<Record4<String, String, Double, Double>> getCSMoneyPriceList() {
@@ -116,31 +115,7 @@ public class QRCSMoneyPrice extends QueryRepository{
         dsl.update(C_S2_SKIN).set(C_S2_SKIN.PRICE, price).set(C_S2_SKIN.MODIFIED_DATE, LocalDateTime.now(ZoneId.of("Europe/Berlin"))).where(C_S2_SKIN.ID.eq(id)).execute();
     }
 
-  /*  public static void save(String weapon, String title, Condition condition, byte stat , double price, int amount){
-        Integer skinId = dsl.select(C_S2_SKIN.ID).from(C_S2_SKIN).join(STASH_SKIN_HOLDER)
-                .on(C_S2_SKIN.STASH_ID.eq(STASH_SKIN_HOLDER.STASH_ID))
-                .where(STASH_SKIN_HOLDER.WEAPON.eq(weapon))
-                .and(STASH_SKIN_HOLDER.TITLE.eq(title))
-                .and(C_S2_SKIN.CONDITION.eq(condition))
-                .and(C_S2_SKIN.STATTRAK.eq(stat))
-                .fetchOneInto(Integer.class);
 
-        //skip if condition for skins doesnt exist
-        if(skinId == null){
-            return;
-        }
-
-        CSMoneyPriceRecord newRecord = dsl.newRecord(CSMoneyPrice.C_S_MONEY_PRICE);
-        newRecord.setCS2SkinId(skinId);
-        newRecord.setPrice(price);
-        newRecord.setAmount(amount);
-        try{
-            newRecord.store();
-        }catch (Exception e){
-            log.warn("Saving CSMoneyPrice failed for {} | {}  id:{}",weapon,title,skinId);
-        }
-
-    }*/
 
     public static double getSkinPrice(int cs2skinId){
         Double result = dsl.select(C_S2_SKIN.PRICE)
@@ -152,155 +127,7 @@ public class QRCSMoneyPrice extends QueryRepository{
     }
 
 
-    @Deprecated
-    public static double getTradeUpSkinAverageAmountSold(String collName, Condition condition, int tupId) {
-    /*    Double result = dsl.select(avg(V_FULLCS2SKIN.AMOUNT))
-                .from(V_FULLCS2SKIN)
-                .join(TRADE_UP_SKINS)
-                .on(TRADE_UP_SKINS.C_S2_SKIN_ID.eq(V_FULLCS2SKIN.ID))
-                .where(V_FULLCS2SKIN.COLL_NAME.eq(collName))
-                .and(V_FULLCS2SKIN.CONDITION.eq(condition))
-                .and(TRADE_UP_SKINS.TRADE_UP_ID.eq(tupId))
-                .fetchOneInto(Double.class);
-        if (result==null){
-            return -3;
-        }
-        return result;*/
-        return -1;
-    }
 
-    public static double getTradeUpSkinAverageAmountSoldCustom(String collName, Condition condition, int tupId) {
-     /*   Double result = dsl.select(avg(V_FULLCS2SKIN.AMOUNT))
-                .from(V_FULLCS2SKIN)
-                .join(TRADE_UP_SKINS_CUSTOM)
-                .on(TRADE_UP_SKINS_CUSTOM.C_S2_SKIN_ID.eq(V_FULLCS2SKIN.ID))
-                .where(V_FULLCS2SKIN.COLL_NAME.eq(collName))
-                .and(V_FULLCS2SKIN.CONDITION.eq(condition))
-                .and(TRADE_UP_SKINS_CUSTOM.TRADE_UP_CUSTOM_ID.eq(tupId))
-                .fetchOneInto(Double.class);
-        if (result==null){
-            return -3;
-        }
-        return result;*/
-        return -1;
-    }
-
-
-    public static double getTradeUpSkinsAveragePrice(String collName, Condition condition, int tupId) {
-
-        Double result = dsl.select(avg(V_FULLCS2SKIN.PRICE))
-                .from(V_FULLCS2SKIN)
-                .join(TRADE_UP_SKINS)
-                .on(TRADE_UP_SKINS.C_S2_SKIN_ID.eq(V_FULLCS2SKIN.ID))
-                .where(V_FULLCS2SKIN.COLL_NAME.eq(collName))
-                .and(V_FULLCS2SKIN.CONDITION.eq(condition))
-                .and(TRADE_UP_SKINS.TRADE_UP_ID.eq(tupId))
-                .fetchOneInto(Double.class);
-        if (result==null){
-            log.warn("Couldn't get average TradeUpSkins price for: tupId -> " + tupId + " coll -> " + collName + " cond -> " + condition);
-            return -3;
-        }
-        return result;
-    }
-
-
-    public static double getTradeUpSkinsAveragePriceCustom(String collName, Condition condition, int tupId) {
-/*
-        Double result = dsl.select(avg(V_FULLCS2SKIN.PRICE))
-                .from(V_FULLCS2SKIN)
-                .join(TRADE_UP_SKINS_CUSTOM)
-                .on(TRADE_UP_SKINS_CUSTOM.C_S2_SKIN_ID.eq(V_FULLCS2SKIN.ID))
-                .where(V_FULLCS2SKIN.COLL_NAME.eq(collName))
-                .and(V_FULLCS2SKIN.CONDITION.eq(condition))
-                .and(TRADE_UP_SKINS_CUSTOM.TRADE_UP_CUSTOM_ID.eq(tupId))
-                .fetchOneInto(Double.class);
-        if (result==null){
-            log.warn("Couldn't get average TradeUpSkins price for: tupId -> " + tupId + " coll -> " + collName + " cond -> " + condition);
-            return -3;
-        }
-        return result;*/
-        return -1;
-    }
-
-
-
-
-    public static void createTradeUpSkins(){
-        Result<Record> tupAndSettings = getTradeUpAndSettingsBuilder().fetch();
-        processCreateTradeUpSkins(tupAndSettings, false);
-    }
-
-
-
-    private static SelectConditionStep<Record> getTradeUpAndSettingsBuilder(){
-        return dsl.select()
-                .from(V_TUPNSETTINGGS)
-                .leftJoin(TRADE_UP_SKINS)
-                .on(V_TUPNSETTINGGS.ID.eq(TRADE_UP_SKINS.TRADE_UP_ID))
-                .where(TRADE_UP_SKINS.TRADE_UP_ID.isNull());
-    }
-
-
-
-    //TODO check min_price for normal calcuation because -1 etc.
-    private static void processCreateTradeUpSkins(Result<Record> tupAndSettings, boolean deleteTradeUps){
-        tradeupLoop:
-        for(Record tupAndS: tupAndSettings){
-            byte stat = tupAndS.get("stattrak", Byte.class);
-            Rarity rarity = tupAndS.get("rarity", Rarity.class);
-            int tupId = tupAndS.get("id", Integer.class);
-
-            TradeUpSettings settings = TradeUpSettings.deserialize(tupAndS.get("settings", String.class));
-            List<SkinUtils.TradeUpSkinInfo> infos = settings.getTradeUpSkinInfo(rarity, stat);
-            for(SkinUtils.TradeUpSkinInfo info : infos){
-
-                SelectConditionStep<Record1<Double>> subquery = dsl.select(min(V_FULLCS2SKIN.PRICE.mul(1.15)))
-                        .from(V_FULLCS2SKIN)
-                        .where(V_FULLCS2SKIN.COLL_NAME.eq(info.getColl_name()))
-                        .and(V_FULLCS2SKIN.RARITY.eq(info.getRarity()))
-                        .and(V_FULLCS2SKIN.CONDITION.eq(info.getCondition()))
-                        .and(V_FULLCS2SKIN.STATTRAK.eq(info.getStattrak()))
-                        .and(V_FULLCS2SKIN.PRICE.gt(0.0));
-
-
-
-                SelectConditionStep<Record1<Integer>> query = dsl.select(V_FULLCS2SKIN.ID)
-                        .from(V_FULLCS2SKIN)
-                        .where(V_FULLCS2SKIN.COLL_NAME.eq(info.getColl_name()))
-                        .and(V_FULLCS2SKIN.RARITY.eq(info.getRarity()))
-                        .and(V_FULLCS2SKIN.CONDITION.eq(info.getCondition()))
-                        .and(V_FULLCS2SKIN.STATTRAK.eq(info.getStattrak()))
-                        .and(V_FULLCS2SKIN.PRICE.ge(0.0))
-                        .and(V_FULLCS2SKIN.PRICE.le(subquery));
-
-                List<Integer> tupSkinIds = query.fetchInto(Integer.class);
-                //TODO delete tradeups that are not possible beause condition doesnt exist?
-                List<Row2<Integer, Integer>> rowList = tupSkinIds.stream().map(tupSkinId -> row(tupId, tupSkinId)).toList();
-                if(rowList.isEmpty()){
-                    //todo
-                    log.warn(""+settings + " "+rarity+ " "+ stat);
-                    if(deleteTradeUps){
-                        int deleted = dsl.deleteFrom(TradeUp.TRADE_UP).where(TradeUp.TRADE_UP.ID.eq(tupId)).execute();
-                        //TODO should be already deleted
-                        if(deleted!=1){
-                            log.warn("Error deleting TradeUp: "+tupId);
-                        }
-                        log.info("Deleted TradeUP: "+tupId);
-                        continue tradeupLoop;
-                    }
-                }
-
-                try{
-                    int inserted = dsl.insertInto(TRADE_UP_SKINS, TRADE_UP_SKINS.TRADE_UP_ID, TRADE_UP_SKINS.C_S2_SKIN_ID).valuesOfRows(rowList).execute();
-                    if(inserted != tupSkinIds.size()){
-                        throw new RuntimeException("Error inserting TradeUpSkins");
-                    }
-                }catch (Exception e){
-                    throw new RuntimeException("Exception inserting TradeUpSkins");
-                }
-            }
-        }
-    }
 
     public static Result<Record3<Integer, Condition, Byte>> getSkinIdsFromName(String weapon, String title){
         return dsl.select(C_S2_SKIN.ID, C_S2_SKIN.CONDITION, C_S2_SKIN.STATTRAK)
@@ -311,9 +138,5 @@ public class QRCSMoneyPrice extends QueryRepository{
                 .and(STASH_SKIN_HOLDER.TITLE.eq(title))
                 .fetch();
 
-    }
-
-
-
-
+        }
     }
